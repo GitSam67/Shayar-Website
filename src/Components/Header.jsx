b3c386a04f983013dd9bf1e877caff97a6618695
Samanuay <200303105038@paruluniversity.ac.in>
2023-01-12 13:30:52 +0530

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,26 +8,26 @@ export default function Header(){
       <nav className="container mx-auto flex flex-row p-3 md:flex-row items-center">
         <div className="w-1/3 text-left">
             <a className="title-font font-bold text-2xl flex flex-row items-center" href="#starter">
-                <img className="h-14 w-32 rounded-xl border-2 border-purple-900" src="../src/assets/shayarlogo.png" alt="logo" />
+                <img className="h-16 w-36 rounded-xl border-2 border-purple-900" src="../src/assets/shayarlogo.png" alt="logo" />
             </a>
         </div>
         <div id="link" className="w-2/3 md:py-1 md:pl-3 flex flex-row items-center text-base justify-center">
-          <NavHashLink to="#" className="transition delay-40 mr-7 font-bold font-mono text-lg text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800 duration-500">
+          <NavHashLink to="#" className="transition delay-40 mr-7 font-bold font-mono text-xl text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800 duration-500">
             Home
           </NavHashLink>
-          <NavHashLink to="#about" className="transition delay-40 mr-7 font-bold font-mono text-lg text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
+          <NavHashLink to="#about" className="transition delay-40 mr-7 font-bold font-mono text-xl text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
             Shayar App
           </NavHashLink>
-          <NavHashLink to="#skills" className="transition delay-40 mr-7 font-bold font-mono text-lg text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
+          <NavHashLink to="#skills" className="transition delay-40 mr-7 font-bold font-mono text-xl text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
             Shayari
           </NavHashLink>
-          <NavHashLink to="#education" className="transition delay-40 mr-7 font-bold font-mono text-lg text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
+          <NavHashLink to="#education" className="transition delay-40 mr-7 font-bold font-mono text-xl text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
             Open Mics
           </NavHashLink>
-          <NavHashLink to="#projects" className="transition delay-40 mr-7 font-bold font-mono text-lg text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
+          <NavHashLink to="#projects" className="transition delay-40 mr-7 font-bold font-mono text-xl text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
             Contact Us
           </NavHashLink>
-          <Link to="/Shayar-Website/mausam" className="transition delay-40 font-bold font-mono text-lg text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
+          <Link to="/Shayar-Website/mausam" className="transition delay-40 font-bold font-mono text-xl text-indigo-900 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800  duration-500">
             Mausam <sup id="live" className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 via-purple-500 to-pink-500">Live</sup>
           </Link>
         </div>
