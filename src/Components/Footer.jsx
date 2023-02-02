9d8867cf4fa9212e24388ec874295315e693b47b
Samanuay <200303105038@paruluniversity.ac.in>
2023-02-02 14:20:16 +0530

    media responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,34 +6,34 @@ export default function Footer() {
             <div className="container md:px-5 py-5 mx-auto">
                 <div className="flex flex-col sm:flex-row">
                 <div className="md:w-1/3 items-center text-center mx-auto">
-                    <h2 className="sm:text-3xl text-2xl font-medium title-font text-white mb-5">• Shayar Community</h2>
+                    <h2 className="sm:text-3xl text-2xl font-medium title-font text-white mb-5">Shayar Community</h2>
                     <h6 className="text-center text-white mb-10 px-7">Thank you for visiting our community page. Connect with us over socials. <br /><p className="my-5"> Keep Exploring 🚀. . . </p></h6>
                 </div>
 
                 <div className="md:w-1/3 items-center text-center mx-auto">
-                    <h2 className="sm:text-3xl text-2xl font-medium title-font md:mr-10 text-white mb-4">• Quick page links</h2>
+                    <h2 className="sm:text-3xl text-2xl font-medium title-font md:mr-10 text-white mb-4"><i class="fas fa-chevron-right mr-2"></i>Quick page links</h2>
                     <div id="quick" className="flex flex-col text-left md:ml-16 mb-10">
-                    <a className="transition delay-40 title-font font-medium text-white hover:text-purple-500 duration-500" href="#starter"><i class="fas fa-chevron-circle-right"></i> home</a>
-                    <a className="transition delay-40 title-font font-medium text-white py-1 hover:text-purple-500 duration-500" href="#about"><i class="fas fa-chevron-circle-right"></i> genre</a>
-                    <a className="transition delay-40 title-font font-medium text-white py-1 hover:text-purple-500 duration-500" href="#skills"><i class="fas fa-chevron-circle-right"></i> open mics</a>
-                    <a className="transition delay-40 title-font font-medium text-white py-1 hover:text-purple-500 duration-500" href="#education"><i class="fas fa-chevron-circle-right"></i> traction</a>
-                    <a className="transition delay-40 title-font font-medium text-white py-1 hover:text-purple-500 duration-500" href="#projects"><i class="fas fa-chevron-circle-right"></i> testimonial</a>
-                    <a className="transition delay-40 title-font font-medium text-white py-1 hover:text-purple-500 duration-500" href="#contact"><i class="fas fa-chevron-circle-right"></i> FAQ's</a>
+                    <a className="transition delay-40 title-font font-medium text-white w-fit hover:text-purple-500 duration-300" href="#starter"><i class="fas fa-chevron-circle-right"></i> home</a>
+                    <a className="transition delay-40 title-font font-medium text-white py-1 w-fit hover:text-purple-500 duration-300" href="#about"><i class="fas fa-chevron-circle-right"></i> genre</a>
+                    <a className="transition delay-40 title-font font-medium text-white py-1 w-fit hover:text-purple-500 duration-300" href="#skills"><i class="fas fa-chevron-circle-right"></i> open mics</a>
+                    <a className="transition delay-40 title-font font-medium text-white py-1 w-fit hover:text-purple-500 duration-300" href="#education"><i class="fas fa-chevron-circle-right"></i> traction</a>
+                    <a className="transition delay-40 title-font font-medium text-white py-1 w-fit hover:text-purple-500 duration-300" href="#projects"><i class="fas fa-chevron-circle-right"></i> testimonial</a>
+                    <a className="transition delay-40 title-font font-medium text-white py-1 w-fit hover:text-purple-500 duration-300" href="#contact"><i class="fas fa-chevron-circle-right"></i> FAQ's</a>
                     </div>
                 </div>
 
                 <div class="md:w-1/3 items-center text-white mx-auto">
-                    <h2 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">• Contact info</h2>
+                    <h2 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">Contact info</h2>
                     <p className="py-1"> <i class="fas fa-phone text-purple-500 mr-2"></i><a href="tel:+91 704-350-7789">+91 704-350-7789</a></p>
                     <p className="py-1"> <i class="fas fa-envelope text-purple-500 mr-2 "></i><a href="mailto:info@shayar.me">info@shayar.me</a></p>
                     <p className="py-1"> <i class="fas fa-map-marked-alt text-purple-500 mr-2"></i><a href="https://google.com">G-405,Darshanam Plaza,<br /> Danteshwar,Vadodara,<br /> India-390004</a></p>
                     
                     <div id="social" class="flex flex-wrap">
-                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-500" href="https://www.linkedin.com/in/samanuay-nambiar-31852b233/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i> </a>
-                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-500" href="https://github.com/GitSam67" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i> </a>
-                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-500" href="mailto:samanuaynr67@gmail.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i> </a>
-                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-500" href="https://twitter.com/SamanuayN" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i> </a>
-                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-500" href="https://dev.to/devsam67" target="_blank" rel="noopener noreferrer"><i class="fab fa-dev"></i> </a>
+                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-400" href="https://www.linkedin.com/in/samanuay-nambiar-31852b233/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i> </a>
+                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-400" href="https://github.com/GitSam67" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i> </a>
+                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-400" href="mailto:samanuaynr67@gmail.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i> </a>
+                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-400" href="https://twitter.com/SamanuayN" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i> </a>
+                    <a className="transition delay-40 rounded-xl h-4 w-4 text-center m-4 mt-2 ml-0 text-white hover:text-purple-500 duration-400" href="https://dev.to/devsam67" target="_blank" rel="noopener noreferrer"><i class="fab fa-dev"></i> </a>
                     </div>
                 </div>
                 </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,12 +6,12 @@ export default function Home() {
     <section id="home">
       <div className="container mx-auto mt-20 flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow w-full mb-16 md:mb-0 flex flex-col mx-auto items-center text-center">
-          <h1 className="title-font sm:text-3xl md:text-4xl mb-4 font-black font-serif text-pink-500">
+          <h1 className="title-font text-3xl md:text-4xl mb-4 font-black font-serif text-pink-500">
             Welcome to <br/>
             <span className="text-purple-600"> India's budding community for Shayar's </span> 
             <br className="hidden lg:inline-block" /><br className="hidden lg:inline-block" />
           </h1>
-          <h3 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 via-purple-600 to-pink-600 title-font sm:text-3xl md:text-4xl mb-4 font-black">
+          <h3 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 via-purple-600 to-pink-600 title-font text-3xl md:text-4xl mb-4 font-black">
             <TypeAnimation
               sequence={[
                 'Shayar App', 
